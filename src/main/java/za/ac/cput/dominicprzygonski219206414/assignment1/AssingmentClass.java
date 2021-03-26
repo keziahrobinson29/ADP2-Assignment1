094dbfd5dda31fe8f5edbf8af403965efd6d4377
Dominic Przygonski <domprzygonski@gmail.com>
2021-03-26 19:48:16 +0200

    First commit creation of blank documennt

---
+++ src/main/java/za/ac/cput/dominicprzygonski219206414/assignment1/AssingmentClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.dominicprzygonski219206414.assignment1;
+
+/**
+ *
+ * @author Dominic Przygonski - 219206414
+ */
+public class AssingmentClass {
+    
+}
